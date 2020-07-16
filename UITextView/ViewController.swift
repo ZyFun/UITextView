83eb184af4385614ca6ec631574da56500579ee6
Dmitriy Danilin <danilindn@icloud.com>
2020-07-16 17:41:43 +0300

    Initial Commit

---
+++ UITextView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITextView
+//
+//  Created by Дмитрий Данилин on 16.07.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
